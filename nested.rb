5ffadc06a3728b641a0794a9cf751a2734e83cec
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-29 14:22:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[yukihiro_matsumoto: {}]
+yukihiro_matsumoto[known_for: :Ruby, languages: ["Lisp", "C"]]
     
 end
 
